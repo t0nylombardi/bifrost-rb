070232333dd28a75704b91bbabeaf2d02746af88
Anthony Lombardi <alombardi.331@gmail.com>
2026-02-16 21:30:47 -0500

    feat: Enhance template renderer to generate domain registration and handler templates
    Add functionality to the TemplateRenderer for generating domain registration and command/query handler templates. This improves the scaffolding process for new domains by ensuring necessary files are created. Update tests to verify the generation of these new templates.

--- lib/bifrost/generators/template_renderer.rb
+++ lib/bifrost/generators/template_renderer.rb
@@ -4,7 +4,8 @@ require "erb"
 
 module Bifrost
   module Generators
-    # Renders starter command/query classes for a generated domain.
+    # Renders starter command/query/handler and domain registration templates
+    # for a generated domain.
     #
     # Templates are intentionally minimal and are only written when target files
     # do not already exist, preserving user code by default.
@@ -25,17 +26,34 @@ module Bifrost
       #
       # @return [void]
       def call
-        generate_create
-        generate_get
-
-        return unless @full
-
-        generate_update
-        generate_delete
-        generate_list
+        base_steps.each { |step| send(step) }
+        full_steps.each { |step| send(step) } if @full
       end
 
       private
+
+      # Defines template generators executed for all invocations.
+      #
+      # @return [Array<Symbol>]
+      def base_steps
+        %i[
+          generate_create
+          generate_get
+          generate_register
+          generate_handlers
+        ]
+      end
+
+      # Defines optional template generators executed when `@full` is true.
+      #
+      # @return [Array<Symbol>]
+      def full_steps
+        %i[
+          generate_update
+          generate_delete
+          generate_list
+        ]
+      end
 
       # Writes file content unless file already exists.
       #
@@ -86,6 +104,89 @@ module Bifrost
                   end
 
                   attr_reader :id
+                end
+              end
+            end
+          RUBY
+        )
+      end
+
+      # Generates per-domain registration helper that wires commands and
+      # queries to their handlers.
+      #
+      # @return [void]
+      def generate_register
+        write_file(
+          "app/domains/#{@context.plural}/register.rb",
+          <<~RUBY
+            module Domains
+              module #{@context.module_name}
+                def self.register(config, repo:)
+                  config.register_command(
+                    Commands::#{@context.module_name}::Create#{@context.class_name},
+                    Handlers::#{@context.module_name}::Create#{@context.class_name}Handler.new(repo: repo)
+                  )
+
+                  config.register_query(
+                    Queries::#{@context.module_name}::Get#{@context.class_name},
+                    Handlers::#{@context.module_name}::Get#{@context.class_name}Handler.new(repo: repo)
+                  )
+                end
+              end
+            end
+          RUBY
+        )
+      end
+
+      # Generates all base command/query handler templates.
+      #
+      # @return [void]
+      def generate_handlers
+        generate_create_handler
+        generate_get_handler
+      end
+
+      # Generates `Create*Handler` command handler skeleton.
+      #
+      # @return [void]
+      def generate_create_handler
+        write_file(
+          "app/handlers/#{@context.plural}/create_#{@context.singular}_handler.rb",
+          <<~RUBY
+            module Handlers
+              module #{@context.module_name}
+                class Create#{@context.class_name}Handler
+                  def initialize(repo:)
+                    @repo = repo
+                  end
+
+                  def call(command)
+                    @repo.create(command.attrs)
+                  end
+                end
+              end
+            end
+          RUBY
+        )
+      end
+
+      # Generates `Get*Handler` query handler skeleton.
+      #
+      # @return [void]
+      def generate_get_handler
+        write_file(
+          "app/handlers/#{@context.plural}/get_#{@context.singular}_handler.rb",
+          <<~RUBY
+            module Handlers
+              module #{@context.module_name}
+                class Get#{@context.class_name}Handler
+                  def initialize(repo:)
+                    @repo = repo
+                  end
+
+                  def call(query)
+                    @repo.find(query.id)
+                  end
                 end
               end
             end
--- spec/bifrost/generators/template_renderer_spec.rb
+++ spec/bifrost/generators/template_renderer_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Bifrost::Generators::TemplateRenderer do
       Dir.mktmpdir do |root|
         FileUtils.mkdir_p(File.join(root, "app/commands/users"))
         FileUtils.mkdir_p(File.join(root, "app/queries/users"))
+        FileUtils.mkdir_p(File.join(root, "app/handlers/users"))
+        FileUtils.mkdir_p(File.join(root, "app/domains/users"))
         context = Bifrost::Generators::NamingContext.new("users")
 
         described_class.new(context, root: root, full: false).call
@@ -26,20 +28,70 @@ RSpec.describe Bifrost::Generators::TemplateRenderer do
       end
     end
 
+    it "generates domain register template" do
+      Dir.mktmpdir do |root|
+        FileUtils.mkdir_p(File.join(root, "app/commands/users"))
+        FileUtils.mkdir_p(File.join(root, "app/queries/users"))
+        FileUtils.mkdir_p(File.join(root, "app/handlers/users"))
+        FileUtils.mkdir_p(File.join(root, "app/domains/users"))
+        context = Bifrost::Generators::NamingContext.new("users")
+
+        described_class.new(context, root: root, full: false).call
+
+        register_file = File.join(root, "app/domains/users/register.rb")
+
+        expect(File).to exist(register_file)
+        expect(File.read(register_file)).to include("module Domains")
+        expect(File.read(register_file)).to include("def self.register(config, repo:)")
+      end
+    end
+
+    it "generates handler skeletons" do
+      Dir.mktmpdir do |root|
+        FileUtils.mkdir_p(File.join(root, "app/commands/users"))
+        FileUtils.mkdir_p(File.join(root, "app/queries/users"))
+        FileUtils.mkdir_p(File.join(root, "app/handlers/users"))
+        FileUtils.mkdir_p(File.join(root, "app/domains/users"))
+        context = Bifrost::Generators::NamingContext.new("users")
+
+        described_class.new(context, root: root, full: false).call
+
+        create_handler = File.join(root, "app/handlers/users/create_user_handler.rb")
+        get_handler = File.join(root, "app/handlers/users/get_user_handler.rb")
+
+        expect(File).to exist(create_handler)
+        expect(File).to exist(get_handler)
+        expect(File.read(create_handler)).to include("class CreateUserHandler")
+        expect(File.read(get_handler)).to include("class GetUserHandler")
+      end
+    end
+
     it "does not overwrite existing files" do
       Dir.mktmpdir do |root|
         create_file = File.join(root, "app/commands/users/create_user.rb")
         get_file = File.join(root, "app/queries/users/get_user.rb")
+        create_handler = File.join(root, "app/handlers/users/create_user_handler.rb")
+        get_handler = File.join(root, "app/handlers/users/get_user_handler.rb")
+        register_file = File.join(root, "app/domains/users/register.rb")
         FileUtils.mkdir_p(File.dirname(create_file))
         FileUtils.mkdir_p(File.dirname(get_file))
+        FileUtils.mkdir_p(File.dirname(create_handler))
+        FileUtils.mkdir_p(File.dirname(get_handler))
+        FileUtils.mkdir_p(File.dirname(register_file))
         File.write(create_file, "existing create")
         File.write(get_file, "existing get")
+        File.write(create_handler, "existing create handler")
+        File.write(get_handler, "existing get handler")
+        File.write(register_file, "existing register")
         context = Bifrost::Generators::NamingContext.new("users")
 
         described_class.new(context, root: root, full: false).call
 
         expect(File.read(create_file)).to eq("existing create")
         expect(File.read(get_file)).to eq("existing get")
+        expect(File.read(create_handler)).to eq("existing create handler")
+        expect(File.read(get_handler)).to eq("existing get handler")
+        expect(File.read(register_file)).to eq("existing register")
       end
     end
 
@@ -47,6 +99,8 @@ RSpec.describe Bifrost::Generators::TemplateRenderer do
       Dir.mktmpdir do |root|
         FileUtils.mkdir_p(File.join(root, "app/commands/users"))
         FileUtils.mkdir_p(File.join(root, "app/queries/users"))
+        FileUtils.mkdir_p(File.join(root, "app/handlers/users"))
+        FileUtils.mkdir_p(File.join(root, "app/domains/users"))
         context = Bifrost::Generators::NamingContext.new("users")
 
         expect { described_class.new(context, root: root, full: true).call }.not_to raise_error
